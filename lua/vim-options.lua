ffcb91166aaa7eb9f78f65f542757e9b5bdab46c
Pedro Filho <pedro@filho.me>
2024-02-07 08:34:43 -0300

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,20 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+-- Navigate through file faster
+vim.keymap.set('n', 'J', '5j')
+vim.keymap.set('n', 'K', '5k')
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
+
+
